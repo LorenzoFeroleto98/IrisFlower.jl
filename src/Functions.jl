61d17e16287776abfd3496a1cb163350308b5625
Lorenzo <lorenzo.feroleto.dev@gmail.com>
2020-03-14 19:30:01 +0100

    Finished to add main tasks

--- src/Functions.jl
+++ src/Functions.jl
@@ -8,3 +8,11 @@ plot(iris, x=:Species, y=:PetalLength, Geom.boxplot)
 plot(iris, x=:PetalLength, color=:Species, Geom.histogram)
 #Histogram PetalWidth
 plot(iris, x=:PetalWidth, color=:Species, Geom.histogram)
+
+CSV.write("test_data.csv", test_data)
+CSV.read("test_data.csv")
+Feather.write("test_data.feather", test_data)
+Feather.read("test_data.feather")
+
+
+test_data = iris[rand(150) .<= 0.1, [:PetalLength, :PetalWidth, :Species]]
